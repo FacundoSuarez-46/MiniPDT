212eec0815539b3dbb4f8c715fdc3e50d0d72169
unknown <frcosati@hotmail.com>
2022-08-16 12:24:16 -0300

    Act roles y funcionalidades

--- src/com/principal/Main.java
+++ src/com/principal/Main.java
@@ -1,6 +1,7 @@
 package com.principal;
 
 import com.formdev.flatlaf.FlatLightLaf;
+import com.proyecto.*;
 import javax.swing.UIManager;
 import ui.Login;
 
@@ -14,6 +15,19 @@ public class Main {
         }
 
         Login login = new Login();
+        
+        Persona p1 = new Persona("5000111-2", "Perez", "Silva", "Juan", "Pedro", date1,"111222hola", "prueba1@utec.com", null);
+		
+		Rol rAdmin = new Rol("Administrador de Sistemas", "Encargado de administrar el sistema", null, null);
+		Rol rJefe = new Rol("Jefe de Sección", "Encargado de gestionar las secciones", null, null);
+		Rol rOperador = new Rol("Operador de Sección", "Se encarga de operar en las secciones", null, null);
+		
+		Funcionalidad fControl = new Funcionalidad("Control de inventario", "Se analiza y controla el stock de inventario", null);
+		Funcionalidad fVentas = new Funcionalidad("Ventas", "Registro de ventas", null);
+		Funcionalidad fCompras = new Funcionalidad("Compras", "Registro de compras", null);
+		Funcionalidad fCuenta = new Funcionalidad ("Cuenta corrientes", "Registro de las cuentas corrientes", null);
+		Funcionalidad fSueldos = new Funcionalidad ("Sueldos", "Registro de sueldos", null);
+        
     }
 
 }
