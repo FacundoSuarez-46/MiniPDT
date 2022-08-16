448c483c170cadc13b270e2e11e1597c8506fc02
unknown <rodrigomcaraballo8@gmail.com>
2022-08-16 10:14:56 -0300

    actualizacion clases

--- src/com/proyecto/Rol.java
+++ src/com/proyecto/Rol.java
@@ -11,5 +11,45 @@ public class Rol {
 	LinkedList<Persona> personasJefe = new LinkedList<Persona>();
 	LinkedList<Persona> personasOperador = new LinkedList<Persona>();
 	
+	public Rol(String nombre, String descripcion, LinkedList<Persona> personasAdministrador,
+			LinkedList<Persona> personasJefe, LinkedList<Persona> personasOperador) {
+		super();
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.personasAdministrador = personasAdministrador;
+		this.personasJefe = personasJefe;
+		this.personasOperador = personasOperador;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public LinkedList<Persona> getPersonasAdministrador() {
+		return personasAdministrador;
+	}
+	public void setPersonasAdministrador(LinkedList<Persona> personasAdministrador) {
+		this.personasAdministrador = personasAdministrador;
+	}
+	public LinkedList<Persona> getPersonasJefe() {
+		return personasJefe;
+	}
+	public void setPersonasJefe(LinkedList<Persona> personasJefe) {
+		this.personasJefe = personasJefe;
+	}
+	public LinkedList<Persona> getPersonasOperador() {
+		return personasOperador;
+	}
+	public void setPersonasOperador(LinkedList<Persona> personasOperador) {
+		this.personasOperador = personasOperador;
+	}
+	
 	
 }
