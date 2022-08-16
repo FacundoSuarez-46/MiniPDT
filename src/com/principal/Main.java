e9e9291686e44f64ec9f0b965b686b3e44937ae5
unknown <rodrigomcaraballo8@gmail.com>
2022-08-16 11:54:39 -0300

    Persona creada

--- src/com/principal/Main.java
+++ src/com/principal/Main.java
@@ -1,12 +1,21 @@
 package com.principal;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
 import com.proyecto.Persona;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Persona p1 = new Persona("5000111-2", "Perez", "Silva", "Juan", "Pedro", null,"111222hola", "prueba1@utec.com", null);
+	public static void main(String[] args) throws ParseException {
+		String sDate1="31/12/1998";
+		Date date1 = new SimpleDateFormat("dd/MM/yyyy").parse(sDate1);
 
+		Persona p1 = new Persona("5000111-2", "Perez", "Silva", "Juan", "Pedro", date1,"111222hola", "prueba1@utec.com", null);
+		
+	
 	}
 
 }
